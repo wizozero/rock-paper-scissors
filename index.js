1743e9f2a35456f8b594e85c1149fb4342dee94e
wizozero <wizocode@gmail.com>
2024-09-09 18:43:34 +0200

    add ui

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let humanScore = 0
+const rockBtn = document.querySelector('#rock-btn')
+const paperBtn = document.querySelector('#paper-btn')
+const scissorsBtn = document.querySelector('#scissors-btn')
+const messageDiv = document.querySelector('#message-div')
+const scoreDiv = document.querySelector('#score-div')
+const resetBtn = document.querySelector('#reset-btn')
+
+let playerScore = 0
 let computerScore = 0
 
 const choices = {
@@ -24,22 +31,45 @@ function playRound(humanChoice, computerChoice) {
 
 	if (humanChoiceInsensitive === computerChoice) {
 		console.log('Draw!')
+		messageDiv.textContent = 'Draw'
 	} else if (choices[humanChoiceInsensitive].beats === computerChoice) {
-		humanScore++
+		playerScore++
+		messageDiv.textContent = `You win ${choices[humanChoiceInsensitive].message}`
 		console.log(`You win ${choices[humanChoiceInsensitive].message}`)
 	} else {
 		computerScore++
+		messageDiv.textContent = `You lose ${choices[computerChoice].message}`
 		console.log(`You lose ${choices[computerChoice].message}`)
 	}
+	getWinner()
 }
 
-function playGame() {
-	for (let i = 0; i <= 4; i++) {
-		const computerSelection = getComputerChoice()
-		const humanSelection = getHumanChoice()
-		playRound(humanSelection, computerSelection)
+function getWinner() {
+	scoreDiv.textContent = `Player score: ${playerScore} , Computer score: ${computerScore}`
+	if (playerScore >= 5) {
+		scoreDiv.textContent = `Player wins!`
+	} else if (computerScore >= 5) {
+		scoreDiv.textContent = `Computer wins!`
 	}
-	console.log(`Final Score, computer: ${computerScore} vs human: ${humanScore}`)
 }
 
-playGame()
+rockBtn.addEventListener('click', () => {
+	playRound(Object.keys(choices)[0], getComputerChoice())
+})
+
+paperBtn.addEventListener('click', () => {
+	playRound(Object.keys(choices)[1], getComputerChoice())
+})
+
+scissorsBtn.addEventListener('click', () => {
+	playRound(Object.keys(choices)[2], getComputerChoice())
+})
+
+resetBtn.addEventListener('click', () => {
+	messageDiv.textContent = ''
+	scoreDiv.textContent = ''
+	computerScore = 0
+	playerScore = 0
+})
+
+console.log(`Final Score, computer: ${computerScore} vs human: ${playerScore}`)
